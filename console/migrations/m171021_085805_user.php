aea6b053eac4b90889da443ef4e2f26ff3dac78b
Jeerawat Khantawong <jeerawat.k@outlook.com>
2017-10-22 11:16:46 +0700

    Migrations

--- console/migrations/m171021_085805_user.php
+++ console/migrations/m171021_085805_user.php
@@ -2,8 +2,34 @@
 
 use yii\db\Migration;
 
-class m130524_201442_init extends Migration
+class m171021_085805_user extends Migration
 {
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m171021_085805_user cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171021_085805_user cannot be reverted.\n";
+
+        return false;
+    }
+    */
     public function up()
     {
         $tableOptions = null;
